25ded454540fa58ac1dedd0df0066335d9923310
GerardoZuno <gerardozunounitec@gmail.com>
2021-08-14 19:47:32 -0500

    Add styled components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header'
+
+function App() {
+  return (
+    <div>
+      <Header title='Cotizador de Seguros'/>
+    </div>
+  )
+}
+
+export default App
